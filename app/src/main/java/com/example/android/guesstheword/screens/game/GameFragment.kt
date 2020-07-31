f5ef456dc07331c73f6e9b2b2313bc3b97e5a137
lewvi <lew_rip@hotmail.com>
2020-07-31 15:07:23 +0700

    Create the GameViewModel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -22,15 +22,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
+import timber.log.Timber
 
 /**
  * Fragment where the game is played
  */
 class GameFragment : Fragment() {
 
-    //private lateinit var viewModel: GameViewModel
+    private lateinit var viewModel: GameViewModel
 
     // The current word
     private var word = ""
@@ -53,6 +55,8 @@ class GameFragment : Fragment() {
                 container,
                 false
         )
+        Timber.i("Called ViewModelProviders.of")
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
 
         resetList()
         nextWord()
